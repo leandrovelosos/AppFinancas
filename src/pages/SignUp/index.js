55928ccd5d0cf840483cc5d2bd46473bbc156bcf
leandrovelosos <leandrovdsantos96@gmail.com>
2025-01-05 22:25:23 -0300

    projeto appfinancas

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,10 @@
+import react from "react";
+import { View, Text } from "react-native";
+
+export default function SignUp() {
+    return (
+        <View>
+            <Text>Tela de cadastro</Text>
+        </View>
+    )
+}
