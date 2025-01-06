43e891c1703af17a941d40d717ceba701e216b0c
leandrovelosos <leandrovdsantos96@gmail.com>
2025-01-06 11:03:39 -0300

    Tela de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,60 @@
 import react from "react";
-import { View, Text } from "react-native";
+import { Platform } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Background, AreaInput, Container, Input, Logo, Link, LinkText, SubmitButton, SubmitText } from "./styles";
 
 export default function SignIn() {
+
+    const navigation = useNavigation();
+
     return (
-        <View>
-            <Text>Tela de Login</Text>
-        </View>
+
+        <Background>
+
+            <Container
+                behavior={Platform.OS === 'ios' ? 'padding' : ''}
+                enabled
+            >
+
+                <Logo
+                    source={require('../../assets/Logo.png')}
+                />
+
+                <AreaInput>
+
+                    <Input
+                        placeholder='Email'
+
+                    />
+
+                </AreaInput>
+
+                <AreaInput>
+
+                    <Input
+                        placeholder='Senha'
+
+                    />
+
+                </AreaInput>
+
+                <SubmitButton activeOpacity={0.8}>
+
+                    <SubmitText>
+                        Acessar
+                    </SubmitText>
+
+                </SubmitButton>
+
+                <Link onPress={() => navigation.navigate('SignUp')}>
+
+                    <LinkText>
+                        Criar Conta
+                    </LinkText>
+
+                </Link>
+            </Container>
+
+        </Background>
     )
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -13,6 +13,9 @@ function AuthRoutes() {
             <AuthStack.Screen
                 name="SignIn"
                 component={SignIn}
+                options={{
+                    headerShown: false
+                }}
             />
 
             <AuthStack.Screen
