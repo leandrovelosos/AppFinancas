0a2a649384d4d393cc7a8c27c784211d5997316a
leandrovelosos <leandrovdsantos96@gmail.com>
2025-01-09 12:09:17 -0300

    header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,24 @@
 import React, { useContext } from "react";
 import { View, Text, Button } from "react-native";
 import { AuthContext } from "../../contexts/auth";
+import { Background } from "./styles"
+
+import Header from "../../components/Header";
 
 export default function Home() {
     const { signOut, user } = useContext(AuthContext);
     return (
-        <View>
-            <Text>Tela Home</Text>
-            <Text> Bem vindo {user.name}</Text>
-            <Button
-                title="Sair"
-                onPress={() => signOut()}
-            />
-        </View>
+        // <View>
+        //     <Text>Tela Home</Text>
+        //     <Text> Bem vindo {user.name}</Text>
+        //     <Button
+        //         title="Sair"
+        //         onPress={() => signOut()}
+        //     />
+        // </View>
+        <Background>
+            <Header title="Minhas movimentações" />
+        </Background>
+
     )
 }
