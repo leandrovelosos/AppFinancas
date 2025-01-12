038340f9b0d4c3be560a4fcce577390f9c620970
leandrovelosos <leandrovdsantos96@gmail.com>
2025-01-12 09:52:06 -0300

    pagina de perfil

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import { Container, Message, LogoutButton, LogoutText, NewLink, NewText, Name } from './styles'
+import Header from "../../components/Header";
+import { AuthContext } from '../../contexts/auth'
+import { useNavigation } from "@react-navigation/native";
+
+export default function Profile() {
+
+    const { user, signOut } = useContext(AuthContext);
+    const navigation = useNavigation();
+    return (
+        <Container>
+            <Header title={"Meu perfil"} />
+
+            <Message>Bem vindo</Message>
+
+            <Name numberOfLines={1}>
+
+                {user && user.name}
+
+            </Name>
+
+            <NewLink onPress={() => navigation.navigate('Registrar')}>
+                <NewText>Fazer registro</NewText>
+            </NewLink>
+
+            <LogoutButton onPress={() => signOut()}>
+                <LogoutText>Sair</LogoutText>
+            </LogoutButton>
+        </Container>
+    )
+}
--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -3,6 +3,7 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import Home from "../pages/Home";
 import New from "../pages/New";
+import Profile from "../pages/Profile";
 
 
 const AppDrawer = createDrawerNavigator();
@@ -19,7 +20,6 @@ function AppRoutes() {
 
 
                 },
-                drawerActiveBackgroundColor: '#3b3dbf',
                 drawerActiveTintColor: '#FFF',
 
                 drawerInactiveBackgroundColor: '#F0F2FF',
@@ -33,8 +33,13 @@ function AppRoutes() {
             />
 
             <AppDrawer.Screen
-                name="Resgistrar"
+                name="Registrar"
                 component={New}
+            />
+
+            <AppDrawer.Screen
+                name="Perfil"
+                component={Profile}
             />
 
         </AppDrawer.Navigator>
